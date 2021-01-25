b58c958fbb33df4fc3c51a468412c895d62961b8
Nick Toby <nick.toby@artisantechgroup.com>
2021-01-25 11:21:10 -0600

    initial commit

---
+++ src/main/java/com/example/demo401/Demo401Application.java
@@ -0,0 +1,13 @@
+package com.example.demo401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo401Application.class, args);
+	}
+
+}
